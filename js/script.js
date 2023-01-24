f6ba1d9a538f0f7273d33c8d6cf2be6c4e5f53f6
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2023-01-24 01:26:52 +0000

    456

--- js/script.js
+++ js/script.js
@@ -3,23 +3,25 @@
 */
 
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/Assignment-6-JS/sw.js", {
+    scope: "/Assignment-6-JS/",
   })
 }
 
-;("use strict")
+"use strict"
 
-function calculate() {
-  const length = parseInt(document.getElementById("pay").value)
-  const width = parseInt(document.getElementById("pay2").value)
-
-  const area = length * width * 0.18
-  const perimeter = length * width * (1.0 - 0.18)
-  const TAX_RATE = 0.18
-
-  document.getElementById("area").innerHTML =
-    "The governemnt will take: " + area + "$"
-  document.getElementById("perimeter").innerHTML =
-    "your pay will be: " + perimeter + "$"
-}
+const getbear = async (URLAddress) => {
+  try {
+    const request = await fetch(URLAddress);
+    const jsonData = await request.json();
+    console.log(jsonData.url);
+    document.getElementById("api-image").innerHTML =
+      '<img src="' +
+      jsonData.url +
+      '" alt="bear image" class="center" width="200% height ="300%" ' +
+      ">";
+  } catch (err) {
+    console.log(err);
+  }
+};
+getbear("https://random.dog/woof.json");
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var GHPATH = "/ICS2O-PWA-Test"
+var GHPATH = "/Assignment-6-JS"
 var APP_PREFIX = "gppwa_"
 var VERSION = "version_001"
 var URLS = [
